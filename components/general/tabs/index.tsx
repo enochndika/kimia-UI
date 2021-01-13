daf34a4ded6f7f2a8c09ced594b454d8927624e9
enochndika <enochbeloved@gmail.com>
2021-01-14 00:01:03 +0100

    tabs component done

---
+++ components/general/tabs/index.tsx
@@ -0,0 +1,113 @@
+import { ReactNode, useState } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface TabsProps {
+  variant: 1 | 2 | 3;
+  children: any;
+}
+
+interface TabsContent extends Props {
+  id: string;
+}
+
+interface TabContainerProps {
+  onClick: (id: string) => void;
+  id: string;
+  activeTab: any;
+  activeClass: string;
+  inactiveClass: string;
+}
+export const Tabs = ({ children, variant }: TabsProps) => {
+  const [activeTab, setActiveTab] = useState<any>(children[0].props.id);
+
+  const activeClassName =
+    variant === 1
+      ? variants.first.active
+      : variant === 2
+      ? variants.second.active
+      : variant === 3
+      ? variants.three.active
+      : variants.first.active;
+
+  const inactiveClassName =
+    variant === 1
+      ? variants.first.inactive
+      : variant === 2
+      ? variants.second.inactive
+      : variant === 3
+      ? variants.three.inactive
+      : variants.first.inactive;
+
+  const onClickTabItem = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <ol>
+        {children.map((child) => {
+          const { id } = child.props;
+          return (
+            <Tabs.Container
+              activeTab={activeTab}
+              key={id}
+              id={id}
+              onClick={onClickTabItem}
+              activeClass={activeClassName}
+              inactiveClass={inactiveClassName}
+            />
+          );
+        })}
+      </ol>
+      <div className="pt-6">
+        {children.map((child) => {
+          if (child.props.id !== activeTab) return undefined;
+          return child.props.children;
+        })}
+      </div>
+    </div>
+  );
+};
+
+Tabs.Container = ({
+  onClick,
+  id,
+  activeTab,
+  activeClass,
+  inactiveClass,
+}: TabContainerProps) => {
+  const isClick = () => {
+    onClick(id);
+  };
+
+  let className = inactiveClass;
+  if (activeTab === id) {
+    className = activeClass;
+  }
+
+  return (
+    <li className={className} onClick={isClick}>
+      {id}
+    </li>
+  );
+};
+
+Tabs.Content = ({ id, children }: TabsContent) => <div id={id}>{children}</div>;
+
+const variants = {
+  first: {
+    active: `text-gray-700  border-b-2 border-indigo-700 -mb-2 px-4 md:px-8 text-sm  py-4 md:py-1 inline-block `,
+    inactive: `-mb-2 px-4 md:px-8 text-sm  py-4 md:py-1 inline-block border-b transition ease-linear duration-200`,
+  },
+  second: {
+    active: `inline-block py-2 px-8 border-solid  border-gray-300 border-t bg-white border-b-0 border-l border-r text-blue-700`,
+    inactive: "inline-block py-2 px-8 text-gray-800 border-b px-8",
+  },
+  three: {
+    active: `text-white py-2 px-8 bg-indigo-900 inline-block transition ease-linear duration-200`,
+    inactive: `-mb-2 py-2 px-8 bg-white text-black  inline-block transition ease-linear duration-200`,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import DefaultButtons from "../components/general/button/defaultButton";
-import { TooltipComponent } from "../components/general/tooltip";
+import { Tabs } from "../components/general/tabs";
 
 export default function Home() {
   return (
@@ -10,7 +10,75 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DefaultButtons />
-      <TooltipComponent />
+      <h2 className="font-bold text-gray-600 text-lg md:text-2xl mt-12 mb-6">
+        Variant 1
+      </h2>
+      <Tabs variant={1}>
+        <Tabs.Content id="React">
+          React has been designed from the start for gradual adoption, and you
+          can use as little or as much React as you need. Whether you want to
+          get a taste of React, add some interactivity to a simple HTML page, or
+          start a complex React-powered app, the links in this section will help
+          you get started
+        </Tabs.Content>
+        <Tabs.Content id="Next.js">
+          The React Framework for Production Next.js gives you the best
+          developer experience with all the features you need for production:
+          hybrid static & server rendering, TypeScript support, smart bundling,
+          route pre-fetching, and more. No config needed.
+        </Tabs.Content>
+        <Tabs.Content id="Nuxt.js">
+          The Intuitive Vue Framework Build your next Vue.js application with
+          confidence using NuxtJS. An open source framework making web
+          development simple and powerful.
+        </Tabs.Content>
+      </Tabs>
+      <h2 className="font-bold text-gray-600 text-lg md:text-2xl mt-20 mb-6">
+        Variant 2
+      </h2>
+      <Tabs variant={2}>
+        <Tabs.Content id="React">
+          React has been designed from the start for gradual adoption, and you
+          can use as little or as much React as you need. Whether you want to
+          get a taste of React, add some interactivity to a simple HTML page, or
+          start a complex React-powered app, the links in this section will help
+          you get started
+        </Tabs.Content>
+        <Tabs.Content id="Next.js">
+          The React Framework for Production Next.js gives you the best
+          developer experience with all the features you need for production:
+          hybrid static & server rendering, TypeScript support, smart bundling,
+          route pre-fetching, and more. No config needed.
+        </Tabs.Content>
+        <Tabs.Content id="Nuxt.js">
+          The Intuitive Vue Framework Build your next Vue.js application with
+          confidence using NuxtJS. An open source framework making web
+          development simple and powerful.
+        </Tabs.Content>
+      </Tabs>
+      <h2 className="font-bold text-gray-600 text-lg md:text-2xl mt-20 mb-6">
+        Tab as Pill
+      </h2>
+      <Tabs variant={3}>
+        <Tabs.Content id="React">
+          React has been designed from the start for gradual adoption, and you
+          can use as little or as much React as you need. Whether you want to
+          get a taste of React, add some interactivity to a simple HTML page, or
+          start a complex React-powered app, the links in this section will help
+          you get started
+        </Tabs.Content>
+        <Tabs.Content id="Next.js">
+          The React Framework for Production Next.js gives you the best
+          developer experience with all the features you need for production:
+          hybrid static & server rendering, TypeScript support, smart bundling,
+          route pre-fetching, and more. No config needed.
+        </Tabs.Content>
+        <Tabs.Content id="Nuxt.js">
+          The Intuitive Vue Framework Build your next Vue.js application with
+          confidence using NuxtJS. An open source framework making web
+          development simple and powerful.
+        </Tabs.Content>
+      </Tabs>
     </>
   );
 }
