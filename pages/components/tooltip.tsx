de5b8afb6fa959d50530e11c8270e633b3b6ae35
enochndika <enochbeloved@gmail.com>
2021-01-14 02:52:19 +0100

    adding tooltip page

---
+++ pages/components/tooltip.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import { TooltipComponent } from "../../components/general/tooltip";
+import MobileLayout from "../../site/layout/mobile";
+import { Container } from "../../site/container";
+
+export default function TooltipPage() {
+  return (
+    <>
+      <Head>
+        <title>Tooltip</title>
+      </Head>
+      <Container>
+        <h1 className="text-center text-3xl md:text-5xl font-bold text-gray-600 mb-16 mt-12">
+          Tooltip
+        </h1>
+        <TooltipComponent />
+      </Container>
+    </>
+  );
+}
+
+TooltipPage.Layout = MobileLayout;
