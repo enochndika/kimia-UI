d10aa244e9694e8d98e1b5b50439307bf7bcf5be
enochndika <enochbeloved@gmail.com>
2021-02-26 05:10:18 +0100

    fix codewrapper.tsx classname

--- website/codeWrapper.tsx
+++ website/codeWrapper.tsx
@@ -1,6 +1,6 @@
 export function CodeWrapper({ children }) {
   return (
-    <div className="h-xl text-xs overflow-y-scroll text-white md:16 md:mb-20">
+    <div className="h-xl text-xs overflow-y-scroll text-white mb-20 md:mb-20">
       <div className="relative text-left box-border p-0 overflow-hidden whitespace-pre font-mono">
         {children}
       </div>
