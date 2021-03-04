c0dfc47a02cf5d33347bd9f290340ad1fb13f163
enochndika <enochbeloved@gmail.com>
2021-03-04 11:30:19 +0100

    update collapse example and snippets

--- packages/collapse/example/index.tsx
+++ packages/collapse/example/index.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 import { Collapse } from '../../collapse';
 
 export const CollapseComponent: FC = () => (
-  <div className="mb-12">
-    <div className="mb-6">
+  <div className="flex flex-wrap mb-4">
+    <div className="mr-5">
       <Collapse title="Click to see me" btnClassName="bg-blue-700">
         <h2>Next.js</h2>
         <h3>React</h3>
@@ -11,14 +11,14 @@ export const CollapseComponent: FC = () => (
         <h5>Chakra-UI</h5>
       </Collapse>
     </div>
-    <div className="mb-6 ">
+    <div className="mr-5">
       <Collapse title="Click to see me" btnClassName="bg-green-700">
         <h2>Tailwindcss</h2>
         <h3>Vercel</h3>
         <h4>Docker</h4>
       </Collapse>
     </div>
-    <div className="mb-6">
+    <div className="mr-5">
       <Collapse title="Click to see me" btnClassName="bg-indigo-900">
         <h1>Netlify</h1>
         <h2>Typescript</h2>
