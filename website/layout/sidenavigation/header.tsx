74f29e4f910a665d29b3d423625ad6fadf394cae
enochndika <enochbeloved@gmail.com>
2021-08-18 00:07:26 +0100

    change sidebar header image

--- website/layout/sidenavigation/header.tsx
+++ website/layout/sidenavigation/header.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import kimiaImg from '/@/public/images/kimia.png';
+import kimiaImg from '@/public/images/kimia.png';
 
 const SidenavHeader = () => (
   <div className="top-0 flex mb-4 pl-4 bg-white lg:hidden">
