51222b8c748387acd2500bbc948c04db4235dbf9
enochndika <enochbeloved@gmail.com>
2021-08-18 00:03:50 +0100

    add kimia logo in sidebar

--- website/layout/sidenavigation/header.tsx
+++ website/layout/sidenavigation/header.tsx
@@ -1,14 +1,9 @@
 import Image from 'next/image';
+import kimiaImg from '/@/public/images/kimia.png';
 
 const SidenavHeader = () => (
   <div className="top-0 flex mb-4 pl-4 bg-white lg:hidden">
-    <Image
-      src="/kimia.png"
-      width={100}
-      height={100}
-      priority={true}
-      alt="kimia-UI"
-    />
+    <Image src={kimiaImg} width={100} height={100} alt="kimia-UI" />
   </div>
 );
 
