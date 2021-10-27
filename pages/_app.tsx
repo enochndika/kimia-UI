829bb7a4959e81b4becc1a70d3ccb2f90a28a55f
enochndika <enochbeloved@gmail.com>
2021-10-27 18:24:17 +0100

    update google analytics to use Script from next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import 'tailwindcss/tailwind.css';
 import dynamic from 'next/dynamic';
 import { AppProps } from 'next/app';
@@ -12,6 +13,7 @@ import * as gtag from '@/website/utils/gtag';
 import { CopyboardProps } from '@/website/components/copyboard';
 import globalMetaTags from '@/website/data/globalMetaTags.json';
 import SkeletonLoader from '@/website/components/skeleton-loader';
+import { GA_TRACKING_ID } from '@/website/utils/gtag';
 
 const Copyboard: React.ComponentType<CopyboardProps> = dynamic(
   () => import('@/website/components/copyboard'),
@@ -47,6 +49,27 @@ function MyApp({ Component, pageProps }: AppProps) {
         {globalMetaTags.map((tag) => (
           <meta property={tag.property} content={tag.content} key={tag.id} />
         ))}
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <MDXProvider components={components}>
         <Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Favicons from '@/website/components/favicons';
-import { GA_TRACKING_ID } from '@/website/utils/gtag';
 
 export default class MyDocument extends Document {
   render() {
@@ -14,22 +13,6 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap"
             rel="stylesheet"
-          />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
           />
           <Favicons />
           <link rel="manifest" href="/favicon/manifest.json" />
