17f7d37d264696f4ab3d731551acffa694cbb17e
enochndika <enochbeloved@gmail.com>
2021-11-27 00:57:09 +0100

    remove next/script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Favicons from '@/website/components/favicons';
+import { GA_TRACKING_ID } from '@/website/utils/gtag';
 
 export default class MyDocument extends Document {
   render() {
@@ -19,6 +20,22 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
           <meta name="theme-color" content="#ffffff" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
